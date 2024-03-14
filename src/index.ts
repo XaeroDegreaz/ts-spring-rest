9ebfe8791389c49c71c968411110c0fe0d83e412
Jerome Doby <xaerodegreaz@gmail.com>
2024-03-15 02:44:03 +0900

    fix npm packaging, and generate types

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+// created from 'create-ts-index'
+
+export * from './Handlers';
+export * from './decorators/Header';
+export * from './decorators/Headers';
+export * from './decorators/PostMapping';
+export * from './decorators/RequestBody';
+export * from './decorators/RequestParameter';
+export * from './decorators/RequestParameters';
+export * from './requestHandler';
